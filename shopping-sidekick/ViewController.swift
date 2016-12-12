389b2bcdf7be88aba71f07edcc4263d8271966b9
Amritanshu Kajaria <kajaria.amritanshu@gmail.com>
2016-12-11 23:55:53 -0500

    get item details

--- shopping-sidekick/ViewController.swift
+++ shopping-sidekick/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import FirebaseAuth
 import FBSDKCoreKit
+import FirebaseDatabase
 
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
@@ -37,28 +38,131 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     
     
-    func getUserItems(UID:String) -> NSMutableArray{
-
-        var itemList = NSMutableArray()
-        itemList.add("ASIN1")//TODO replace with data from database
-        itemList.add("ASIN2")//TODO replace with data from database
-        itemList.add("ASIN3")//TODO replace with data from database
-        return itemList
-    }
+//    func getUserItems(UID:String) -> NSMutableArray{
+//
+//        var itemList = NSMutableArray()
+//        itemList.add("ASIN1")//TODO replace with data from database
+//        itemList.add("ASIN2")//TODO replace with data from database
+//        itemList.add("ASIN3")//TODO replace with data from database
+//        return itemList
+//    }
     
-    func getItemDetails(ASIN:String) -> NSMutableDictionary{
-
-        var itemDetails = NSMutableDictionary()
-        itemDetails["ASIN"] = ASIN //TODO replace with data from database
-        itemDetails["avg"] = 193.13 //TODO replace with data from database
-        itemDetails["highest"] = 1913.13 //TODO replace with data from database
-        itemDetails["lowest"] = 3.13 //TODO replace with data from database
-        itemDetails["yesterday"] = 242 //TODO replace with data from database
-        itemDetails["today"] = 13.13 //TODO replace with data from database
+//    func getItemDetails(ASIN:String) -> NSMutableDictionary{
+//
+//        var itemDetails = NSMutableDictionary()
+//        itemDetails["ASIN"] = ASIN //TODO replace with data from database
+//        itemDetails["avg"] = 193.13 //TODO replace with data from database
+//        itemDetails["highest"] = 1913.13 //TODO replace with data from database
+//        itemDetails["lowest"] = 3.13 //TODO replace with data from database
+//        itemDetails["yesterday"] = 242 //TODO replace with data from database
+//        itemDetails["today"] = 13.13 //TODO replace with data from database
+//        
+//        return itemDetails
+//        
+//    }
+    
+//    func getUsersItem() -> NSMutableArray
+//    {
+//
+//        
+//    }
+    
         
-        return itemDetails
-        
-    }
+//        
+////
+////            print("hi")
+////            print(value)
+////
+////            for item in value!{
+////                
+////                print(item.key)
+////                
+////                var asin = String(_ describing: item.key) as? String
+////                
+////                
+////                
+////                
+////                let res = getItemDetails(asin)
+////                print(res)
+////                
+////                
+////            }
+//            
+////            print(value ?? "")
+////
+////            
+////            let items = value?.allKeys
+////            
+////            var cnt = 1
+////            for item in items!
+////            {
+////                //var asin = String(describing: item)
+////                var asin:String = item as! String
+////                
+////                
+////                let v = "" + String(asin)
+////                
+////                print(v)
+////                
+////                
+////                getItemDetails(v)
+////                
+////                
+////                
+////                
+////                
+////                
+////                
+////                
+////                
+////                
+////                
+////                print(String(cnt) + " ) " + String(describing: item))
+////                cnt += 1
+////            }
+////            
+////            
+//            
+//            // ...
+////        }) { (error) in
+////            print(error.localizedDescription)
+////        }
+////        
+//        return res
+//    }
+//    
+//    func getItemDetails(asin:String) -> NSMutableDictionary
+//    {
+//        var itemDetails = NSMutableDictionary()
+//        
+//        var ref: FIRDatabaseReference!
+//        let res = NSMutableArray()
+//        ref = FIRDatabase.database().reference()
+//        
+//        
+//        ref.child("items").child(asin).observeSingleEvent(of: .value, with: { (snapshot) in
+//            // Get user value
+//            let value = snapshot.value as? NSMutableDictionary
+//            print(value!); print("IT WOKED")
+//            
+//            itemDetails = value!
+//            
+////            print("I am here")
+//            
+////            print(value!)
+//            
+//            
+//            // ...
+//        }) { (error) in
+//            print(error.localizedDescription)
+//        }
+//        
+//        
+//
+//        
+//    return value
+//    }
+//    
     
     
         override func viewDidLoad() {
@@ -72,9 +176,43 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 
                 self.welcomeLabel.text = "Welcome " + name!
                 
+                
+                let itemDetailsList = NSMutableArray()
+                
+                var ref: FIRDatabaseReference!
+                ref = FIRDatabase.database().reference()
+                
+                
+                ref.child("users").child(uid!).child("subscribedItems").observeSingleEvent(of: .value, with: { (snapshot) in
+                    
+                    let value = snapshot.value as? NSDictionary
+                    
+                    for item in value!
+                    {
+                        ref.child("items").child(item.key as! String ).observeSingleEvent(of: .value, with: { (snapshot) in
+                            // Get user value
+                            let itemDetails = snapshot.value as? NSMutableDictionary
+                            
+                            itemDetailsList.add(itemDetails as Any)
+                            
+                            print(itemDetails ?? "")
+                        }) { (error) in
+                            print(error.localizedDescription)
+                        }
+                    }
+                    
+                }) { (error) in
+                    print(error.localizedDescription)
+                }
+                
+                print("Printing List")
+                print(itemDetailsList)
+                
             }
             
-            let itemDetailsList = NSMutableArray()
+            
+
+            
             
 //            let items = getUserItems("asja")
             
