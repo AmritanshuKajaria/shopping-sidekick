38dc3b29706ebe74b594b16d8ce02fa1d5fb0675
Amritanshu Kajaria <kajaria.amritanshu@gmail.com>
2016-12-12 15:48:20 -0500

    search Item attempt 1

--- shopping-sidekick/AddItemViewController.swift
+++ shopping-sidekick/AddItemViewController.swift
@@ -9,90 +9,27 @@
 import UIKit
 
 class AddItemViewController: UIViewController, UITextFieldDelegate {
-    
-    @IBOutlet weak var searchField: UITextField!
-
-    var searchInput:String = ""
-    
-    @IBOutlet weak var dummyLabel: UILabel!
-    
-    var searchResults: NSMutableArray! = NSMutableArray()
 
     @IBOutlet weak var tableView: UITableView!
-   
-    
-    var array1 = ["1","2","3"]
+    @IBOutlet weak var searchField: UITextField!
     var searchResult = NSMutableArray()
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        }
+    }
     
     @IBAction func changeTextButton(_ sender: Any) {
-//        dummyLabel.text = searchField.text
-        searchInput = searchField.text!
-        print(searchInput)
         
-        
-        
-        self.searchResult = searchItem(searchTerm: searchInput)
-        
-        print("search count => " + String(self.searchResult.count))
+        let searchTerm = searchField.text!
         searchField.text = nil
-        
-        print("searchResItemList =>")
-        print(self.searchResult)
-        
-        
-//        self.tableView.reloadData()
-        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        
-    }
-    
-    func numberOfSectionsInTableView(_ tableView: UITableView) -> Int
-    {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.searchResult.count;
-    }
-    
-    
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath as IndexPath)
-
-        let itemDetails = (self.searchResult.object(at: indexPath.row) as AnyObject)
-        
-        cell.textLabel?.text = itemDetails["title"] as? String
-        
-        //            print(cell.textLabel?.text)
-        return cell //BACKUP
-        
-        
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        //self.result = self.searchResults.object(at: indexPath.row) as? String
-        //print(self.result)
-      //  self.performSegue(withIdentifier: "resultsSegue", sender: self)
-        
-    }
-    
-    func searchItem(searchTerm:String) -> NSMutableArray   {
-        let resItemList = NSMutableArray()
+        print(searchTerm)
         
         let config = URLSessionConfiguration.default // Session Configuration
         let session = URLSession(configuration: config) // Load configuration into Session
         
         let cleanSearchTerm = searchTerm.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
         let full_url = "https://api.keepa.com/search?key=8fdnui1hbe9d62tc5npkmpgkdsaugfoui8ranves3sj5roe3tj1cfj5fggnbqtoo&domain=1&type=product&term="+cleanSearchTerm!
-        
         
         let url = URL(string: full_url)!
         
@@ -109,46 +46,22 @@ class AddItemViewController: UIViewController, UITextFieldDelegate {
                     
                     if let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
                     {
-                        print("json =>")
-                        print(json)
-                        
-                        print("json.count =>")
-                        print(json.count)
-                        
-                        print("json.keys =>")
-                        print(json.keys)
-                        
-                        let j = json as! NSMutableDictionary
-                        
-                        print("j =>")
-                        print(j)
-                        
-                        print("j.count =>")
-                        print(j.count)
-                        
-                        print("j.keys =>")
-                        print(j.allKeys)
-                        
-                        let products = j["products"] as! NSArray
-                        
-                        print("products.count => ")
-                        print(products.count)
+                        let products = json["products"] as! NSArray
                         
                         for product in products
                         {
                             let p = product as! NSDictionary
-                            
                             let item = NSMutableDictionary()
                             item["title"] = p["title"]
                             item["asin"] = p["asin"]
                             item["image"] = p["imagesCSV"]
-                            resItemList.add(item)
+                            print(item)
+                            self.searchResult.add(item)
                             
                             DispatchQueue.main.async {
                                 self.tableView.reloadData()
                             }
                         }
-                        
                     }
                     
                 } catch {
@@ -162,10 +75,26 @@ class AddItemViewController: UIViewController, UITextFieldDelegate {
             
         })
         task.resume()
-        return resItemList
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.searchResult.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("indexPath => " +  String(describing: indexPath))
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath as IndexPath)
+        let itemDetails = (self.searchResult.object(at: indexPath.row) as AnyObject)
+        cell.textLabel?.text = itemDetails["title"] as! String
+        return cell //BACKUP
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     }
 
-    
-    
-    
 }
